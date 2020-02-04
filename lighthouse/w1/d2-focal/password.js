a7cb58c6ed1d535eb3c2cfb764226c820734a146
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-04 17:20:32 +0000

    working

--- lighthouse/w1/d2-focal/password.js
+++ lighthouse/w1/d2-focal/password.js
@@ -1,5 +1,20 @@
 const args = process.argv;
-let arr = args.slice(2,args.length)
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i])
+let input = args[2];
+let temp = "";
+if (!input) {
+  return console.log("There is no input");
+}
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === "a") {
+    temp += "4";
+  } else if (input[i] === "e") {
+    temp += "3";
+  } else if (input[i] === "o") {
+    temp += "0";
+  } else if (input[i] === "l") {
+    temp += "1";
+  } else {
+    temp += input[i];
+  }
 };
+console.log(temp);
